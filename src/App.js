5a57047ba8a8c9d306b15d308d9a5de26fedfdbc
Hareharun <hareharun@gmail.com>
2023-12-05 11:49:14 +0530

    Initialized basic project setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./app.css";
+import { SfButton, SfCheckbox } from "@storefront-ui/react";
+import FooterComponent from "./components/FooterComponent/FooterComponent";
+
+const App = () => {
+  return (
+    <>
+      {/* <h1 className="text-primary text-4xl font-bold">
+        Hello world! I am using React
+      </h1> */}
+      <FooterComponent />
+    </>
+  );
+};
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+const { tailwindConfig } = require("@storefront-ui/react/tailwind-config");
+module.exports = {
+  presets: [tailwindConfig],
+  content: [
+    "./src/**/*.{js,jsx}",
+    "./public/index.html",
+    "./node_modules/@storefront-ui/react/**/*.{js,mjs}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
